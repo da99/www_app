4f8fe0aa40ea88d0c5f363f1024364fe9d496cf6
da99 <i-hate-spam-1234567@mailinator.com>
2013-03-24 20:50:40 -0500

    Added: name validation of text input. Added: unknown element error.

--- test/ok_slang.js
+++ test/ok_slang.js
@@ -10,9 +10,40 @@ describe( 'ok_slang', function () {
   describe( '.to_html', function () {
 
     it( 'returns a string', function () {
-      var r = Ok.new([{form: [{text_box: ['my_name', "enter name", "one line"]}]}]).to_html();
+      var html = [{
+        form: [
+          {text_box: ['my_name', "enter name", "one line"] } ]
+      }];
+      var r = Ok.new(html).to_html();
       assert.equal(r, "<form><input name=\"my_name\" type=\"text\">enter name</input></form>");
     });
 
+    it( 'throws error if invalid chars in name', function () {
+      var err = null;
+      var html = [{
+        form: [
+          {text_box: ['my name', "enter name", "one line"] } ]
+      }];
+
+      try { Ok.new(html).to_html(); }
+      catch (e) { err = e; }
+
+      assert.equal(err.message, "Invalid chars in name: my name");
+    });
+
+    it( 'throws error if unknown element', function () {
+      var err = null;
+      var html = [{
+        form: [
+          {text_boxy: ['my_name', "enter name", "one line"] } ]
+      }];
+
+      try { Ok.new(html).to_html(); }
+      catch (e) { err = e; }
+
+      assert.equal(err.message, "Unknown element: text_boxy");
+    });
+
   }); // === end desc
+
 }); // === end desc
