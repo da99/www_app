375cedb8751ffe0743cfaffff7dc32cfea972376
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-07 22:36:31 -0500

    Fixed: attr functions were not all adding name to error messages.

--- test/sanitize_attrs.js
+++ test/sanitize_attrs.js
@@ -19,21 +19,41 @@ describe( 'Sanitize attrs:', function () {
         assert.equal(E[name](undefined).constructor, Error);
       });
 
+      if ( !_.contains("name href action".split(' '), name ) )
+        it( 'adds specified name to error', function () {
+          var result = E[name](null, 'my_name').message;
+          if ( result.indexOf('my_name: ') !== 0)
+            assert.fail(result, 'my_name', 'E.' + name + ' is not adding name to error message.');
+        });
+
     }); // === end desc
 
   }); // end _.each
 
+  describe( 'string', function () {
+    it( 'returns value if string', function () {
+      assert.equal(E.string("s"), "s");
+    });
+
+    it( 'returns error if value is number', function () {
+      assert.equal(E.string(1).constructor, Error);
+    });
+  }); // === end desc
+
   describe( 'name', function () {
-    it( 'returns value is valid string', function () {
+    it( 'returns value if valid string', function () {
       assert.equal(E.name("some_name"), "some_name");
     });
   }); // === end desc
 
-  describe( 'href', function () {
-    it( 'returns error if url is not valid', function () {
-      assert.equal(E.href("http://wwwtome<").message, "href: URI is not strictly valid.: http://wwwtome<");
-    });
-  }); // === end desc
+  _.each( ['href', 'action', 'uri'] , function (name) {
+    describe( 'url: ' + name, function () {
+      it( 'returns error if url is not valid', function () {
+        assert.equal(E[name]("http://wwwtome<").message, name + ": URI is not strictly valid.: http://wwwtome<");
+      });
+    }); // === end desc
+  });
+
 }); // === end desc
 
 
