f949e7639c3240f1b0d0568724e298d11d57589b
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-09 21:22:33 -0500

    Fixed: tests for errors updated to use new design.

--- test/www_applet_crud.js
+++ test/www_applet_crud.js
@@ -3,4 +3,19 @@ describe( '.def_tag', function () {
   it( 'turns a null for args def into: { }', function () {
     assert.equal(false, true);
   });
+
+  it( 'accepts an array as an argument instead of array', function () {
+    var args = {val: "anything"};
+    var results = null;
+
+    var app = Applet.new(['box', args, ["some text"]]);
+    app.def_tag('box', ['val'], function (m, tag, attrs, content) {
+      dfsdf()
+      results = attrs;
+      return [tag, attrs, content];
+    });
+    RUN(app);
+
+    assert.equal(results, args);
+  });
 }); // === end desc
--- test/www_applet_errors.js
+++ test/www_applet_errors.js
@@ -37,23 +37,11 @@ describe( 'Errors:', function () {
     assert.equal(ERROR(html).message, "Function not found: text_boxs");
   });
 
-  it( 'accepts a KV object as an argument instead of array', function () {
-    var args = {val: "anything"};
-    var results = null;
-
-    var app = Applet.new(['box', args, ["some text"]]);
-    app.def_tag('box', {val: anything}, null, function (m, tag, attrs, content) {
-      results = attrs;
-      return [tag, attrs, content];
-    });
-    RUN(app);
-
-    assert.equal(results, args);
-  });
 
   it( 'returns error if arguments are numbers instead of array/object', function () {
 
-    var app = Applet.new(['box', 100, []], ['box', function (m, a1, a2) {}]);
+    var app = Applet.new(['box', 100, []]);
+    app.def_tag('box', [], function (m, a1, a2) {});
     app.run();
 
     assert.equal(app.error.message, "box: invalid argument: 100");
