b85611743dff9f810527ac6d8ae3ef16b1056683
da99 <i-hate-spam-1234567@mailinator.com>
2014-05-28 17:37:34 -0500

    Added: json gems, some initial specs.

--- lib/www_applet.rb
+++ lib/www_applet.rb
@@ -1,10 +1,28 @@
 
-class Www_Applet
+require "multi_json"
+
+class WWW_Applet
+
+  Invalid = Class.new(RuntimeError)
 
   class << self
   end # === class self ===
 
-  def initialize
+  attr_reader :code
+
+  def initialize o
+    case o
+    when String
+      @code = o
+      @obj  = MultiJson.load(o)
+    else
+      @code = MultiJson.dump(o)
+      @obj  = o
+    end
+
+    unless @obj.is_a?(Array)
+      fail Invalid.new("JS object must be an array.")
+    end
   end
 
-end # === class Www_Applet ===
+end # === class WWW_Applet ===
--- specs/www_applet.rb
+++ specs/www_applet.rb
@@ -1,8 +1,22 @@
 
-describe "www_applet" do
+describe "WWW_Applet.new" do
 
-  it "runs" do
-    Www_Applet.should.flunk "No tests written."
+  it "accepts a string" do
+    o = WWW_Applet.new "[]"
+    o.code.should == "[]"
   end
 
-end # === describe www_applet ===
+  it "accepts an array" do
+    code = ["a", []]
+    o = WWW_Applet.new code
+    o.code.should == MultiJson.dump(code)
+  end
+
+  it "raises WWW_Applet::Invalid if object is not an Array" do
+    lambda {
+      WWW_Applet.new({a:"a"})
+    }.should.raise(WWW_Applet::Invalid).
+    message.should.match /JS object must be an array/i
+  end
+
+end # === describe WWW_Applet.new ===
