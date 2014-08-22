a9110490a7e333a182e1c64622d42ba20ae18155
da99 <i-hate-spam-1234567@mailinator.com>
2014-08-22 02:15:07 -0500

    Added: bin/css_props

--- lib/www_applet.rb
+++ lib/www_applet.rb
@@ -1,6 +1,4 @@
 
-require 'sanitize'
-
 class WWW_Applet
 
   Classes                     = []
@@ -56,7 +54,44 @@ class WWW_Applet
       :allow_hacks    => false,
       :at_rules       => [ 'font-face', 'media' ],
       :protocols      => [ :relative ],
-      :properties     => Sanitize::Config::RELAXED[:css][:properties]
+      :properties     => [
+        # From: Sanitize::Config::RELAXED[:css][:properties]
+        :align_content, :align_items, :align_self, :animation, :animation_delay, 
+        :animation_direction, :animation_duration, :animation_fill_mode, :animation_iteration_count, :animation_name, :animation_play_state, 
+        :animation_timing_function, :backface_visibility, :background, :background_attachment, :background_clip, :background_color, 
+        :background_image, :background_origin, :background_position, :background_repeat, :background_size, :border, 
+        :border_bottom, :border_bottom_color, :border_bottom_left_radius, :border_bottom_right_radius, :border_bottom_style, :border_bottom_width, 
+        :border_collapse, :border_color, :border_image, :border_image_outset, :border_image_repeat, :border_image_slice, 
+        :border_image_source, :border_image_width, :border_left, :border_left_color, :border_left_style, :border_left_width, 
+        :border_radius, :border_right, :border_right_color, :border_right_style, :border_right_width, :border_spacing, 
+        :border_style, :border_top, :border_top_color, :border_top_left_radius, :border_top_right_radius, :border_top_style, 
+        :border_top_width, :border_width, :bottom, :box_decoration_break, :box_shadow, :box_sizing, 
+        :break_after, :break_before, :break_inside, :caption_side, :clear, :clip, 
+        :clip_path, :color, :column_count, :column_fill, :column_gap, :column_rule, 
+        :column_rule_color, :column_rule_style, :column_rule_width, :column_span, :column_width, :columns, 
+        :content, :counter_increment, :counter_reset, :cursor, :direction, :display, 
+        :empty_cells, :filter, :flex, :flex_basis, :flex_direction, :flex_flow, 
+        :flex_grow, :flex_shrink, :flex_wrap, :float, :font, :font_family, 
+        :font_feature_settings, :font_kerning, :font_language_override, :font_size, :font_size_adjust, :font_stretch, 
+        :font_style, :font_synthesis, :font_variant, :font_variant_alternates, :font_variant_caps, :font_variant_east_asian, 
+        :font_variant_ligatures, :font_variant_numeric, :font_variant_position, :font_weight, :height, :hyphens, 
+        :icon, :image_orientation, :image_rendering, :image_resolution, :ime_mode, :justify_content, 
+        :left, :letter_spacing, :line_height, :list_style, :list_style_image, :list_style_position, 
+        :list_style_type, :margin, :margin_bottom, :margin_left, :margin_right, :margin_top, 
+        :marks, :mask, :mask_type, :max_height, :max_width, :min_height, 
+        :min_width, :object_fit, :object_position, :opacity, :order, :orphans, 
+        :outline, :outline_color, :outline_offset, :outline_style, :outline_width, :overflow, 
+        :overflow_wrap, :overflow_x, :overflow_y, :padding, :padding_bottom, :padding_left, 
+        :padding_right, :padding_top, :page_break_after, :page_break_before, :page_break_inside, :perspective, 
+        :perspective_origin, :position, :quotes, :resize, :right, :tab_size, 
+        :table_layout, :text_align, :text_align_last, :text_combine_horizontal, :text_decoration, :text_decoration_color, 
+        :text_decoration_line, :text_decoration_style, :text_indent, :text_orientation, :text_overflow, :text_rendering, 
+        :text_shadow, :text_transform, :text_underline_position, :top, :touch_action, :transform, 
+        :transform_origin, :transform_style, :transition, :transition_delay, :transition_duration, :transition_property, 
+        :transition_timing_function, :unicode_bidi, :unicode_range, :vertical_align, :visibility, :white_space, 
+        :widows, :width, :word_break, :word_spacing, :word_wrap, :writing_mode, 
+        :z_index
+      ]
     }
 
   }
