39ce60f1ebeccad596c892cb7c3cb32a96b7a116
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-18 14:13:24 -0500

    File rename: script/js

--- specs/as_ruby/0016-script.rb
+++ specs/as_ruby/0016-script.rb
@@ -1,5 +1,5 @@
 
-describe :JS do
+describe :script do
 
   it "renders js" do
     target :script, <<-EOF
@@ -18,5 +18,37 @@ describe :JS do
   end
 
   it "can only have a relative url"
+
+  it "escapes text as :html" do
+    target :script, <<-EOF
+      WWW_Applet.compile(
+        ["create_event",["div","click","add_class",["red&lt;red"]]]
+      );
+    EOF
+
+    actual do
+      div {
+        on(:click) { add_class "red<red" }
+      }
+    end
+  end
+
+  it "does not allow vars in :script :src attribute" do
+    target :body, <<-EOF
+      <script src="help"></script>
+    EOF
+
+    actual do
+      script.src(:help)
+    end
+  end
+
+  it "ignores text for :script block" do
+    target %^<script src="&#47;hello.js"></script>^
+    actual {
+      script.src('/hello.js') { 'hello' }
+    }
+  end
+
 
 end # === describe :JS ===
