ff1af37263c35fc064b72840a287e3e6fced7ccc
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-18 23:05:13 -0500

    Dev. checkpoint: touchup

--- specs/as_ruby/0011-id.rb
+++ specs/as_ruby/0011-id.rb
@@ -3,20 +3,20 @@
 
 describe :* do
 
-  it "adds 'id' attribute: a.*(:warning)(...) { }" do
-    target '<a id="warning" href="&#47;there">There</a>'
-
-    actual do
-      a.*(:warning).href('/there') { "There" }
-    end
-  end
-
   it "raises Invalid if id has invalid chars" do
     should.raise(Escape_Escape_Escape::Invalid) {
       actual do
         div.*('a<&a') { 'hello' }
       end
     }.message.should.match /a<&a/
+  end
+
+  it "adds 'id' attribute: a.*(:warning)(...) { }" do
+    target '<a id="warning" href="&#47;there">There</a>'
+
+    actual do
+      a.*(:warning).href('/there') { "There" }
+    end
   end
 
 end # === describe WWW_Applet.new ===
