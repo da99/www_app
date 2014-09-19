042ad532cc359418d982055ea67c603547cabf5d
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-19 03:39:30 -0500

    Added: implementation of spec.

---
+++ specs/as_ruby/0020-tag.rb
@@ -0,0 +1,9 @@
+
+describe :tag do
+
+  it "raises Invalid if tag starts w/ a number"
+
+  it "raises Invalid if tag is unknown: e.g. :footers"
+
+end # === describe :tag ===
+
--- specs/as_ruby/0021-form.rb
+++ specs/as_ruby/0021-form.rb
@@ -10,7 +10,13 @@ describe :forms do
     }.message.should.match /auth_token/
   end
 
-  it "can only have a relative url"
+  it "raises Invalid_Relative_HREF if :action is not a relative url" do
+    should.raise(Escape_Escape_Escape::Invalid_Relative_HREF) {
+      actual :auth_token => 'mytoken' do
+        form.action('http://www.google.com/') {}
+      end
+    }.message.should.match /google.com/
+  end
 
 end # === describe :forms ===
 
