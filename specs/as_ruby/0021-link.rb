2f2ac86192238bea1520fec8f31a209002666109
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-19 03:43:28 -0500

    Added: implementation of :link specs

--- specs/as_ruby/0021-link.rb
+++ specs/as_ruby/0021-link.rb
@@ -1,6 +1,26 @@
 
 describe :link do
 
-  it "can only have a relative url"
+  it "raises Invalid_Relative_HREF" do
+    should.raise(Escape_Escape_Escape::Invalid_Relative_HREF) {
+      actual do
+        link.href('http://www.google.com/')./
+      end
+    }.message.should.match /google/
+  end
+
+  it "escapes slashes in :href" do
+    target %^<link href="&#47;css&#47;css&#47;styles.css" />^
+    actual {
+      link.href('/css/css/styles.css')./
+    }
+  end
+
+  it "allows a relative :href" do
+    target %^<link href="&#47;css.css" />^
+    actual {
+      link.href('/css.css')./
+    }
+  end
 
 end # === describe :link
