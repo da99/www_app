8d77b4498b386443f07353843d76cc64c0479a9e
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-19 05:39:21 -0500

    Added: tag specs

--- specs/as_ruby/0020-tag.rb
+++ specs/as_ruby/0020-tag.rb
@@ -1,9 +1,21 @@
 
 describe :tag do
 
-  it "raises Invalid if tag starts w/ a number"
+  it "raises Invalid if tag starts w/ a number" do
+    should.raise(Escape_Escape_Escape::Invalid) {
+      actual do
+        div.*('0a') { 'hello' }
+      end
+    }.message.should.match /0a/
+  end
 
-  it "raises Invalid if tag is unknown: e.g. :footers"
+  it "raises Invalid if tag is unknown: e.g. :footers" do
+    should.raise(StandardError) {
+      actual do
+        tag(:footers) { 'bye' }
+      end
+    }.message.should.match /footers/
+  end
 
 end # === describe :tag ===
 
