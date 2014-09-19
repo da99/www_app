a413f1266b4682a18c6c149efab931d20423e7e9
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-19 04:01:04 -0500

    Added: specs for :script

--- specs/as_ruby/0021-script.rb
+++ specs/as_ruby/0021-script.rb
@@ -1,6 +1,42 @@
 
 describe :script do
 
+  it "escapes text as :html" do
+    target :script, <<-EOF
+      WWW_Applet.compile(
+        ["create_event",["div","click","add_class",["red&lt;red"]]]
+      );
+    EOF
+
+    actual do
+      div {
+        on(:click) { add_class "red<red" }
+      }
+    end
+  end
+
+  it "raises Invalid_Relative_HREF if :src is not relative" do
+    should.raise(Escape_Escape_Escape::Invalid_Relative_HREF) {
+      actual do
+        script.src('http://www.example.org/file.js')./
+      end
+    }.message.should.match /example\.org/
+  end
+
+  it "allows a relative :src" do
+    target %^<script src="&#47;file.js"></script>^
+    actual {
+      script.src('/file.js')./
+    }
+  end
+
+  it "escapes slashes in attr :src" do
+    target %^<script src="&#47;dir&#47;file.js"></script>^
+    actual {
+      script.src('/dir/file.js')./
+    }
+  end
+
   it "renders js" do
     target :script, <<-EOF
       WWW_Applet.compile(
@@ -15,22 +51,6 @@ describe :script do
         on(:click) { add_class :hello }
       }
     }
-  end
-
-  it "can only have a relative url"
-
-  it "escapes text as :html" do
-    target :script, <<-EOF
-      WWW_Applet.compile(
-        ["create_event",["div","click","add_class",["red&lt;red"]]]
-      );
-    EOF
-
-    actual do
-      div {
-        on(:click) { add_class "red<red" }
-      }
-    end
   end
 
   it "does not allow vars in :script :src attribute" do
