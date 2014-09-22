d1d1ceeaa312b0b4f622f5bad8ef09c4eae625f7
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-22 02:23:20 -0500

    Started: implementation of client-side js.

--- specs/client-side/index.js
+++ specs/client-side/index.js
@@ -2,7 +2,10 @@
 "use strict";
 
 QUnit.test( "it runs the code", function( assert ) {
-  WWW_Applet.run([""]);
-  assert.ok( 1 === "1", "Passed!" );
+  WWW_Applet.run([
+    "focus on", ['#box_1'],
+    "add class", ['class_1']
+  ]);
+  assert.ok( $('#box_1').hasClass('class_1') === true, "Passed!" );
 });
 
