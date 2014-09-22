c9c60a0262860b6c5e03d31d1a5095ca43295d5f
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-22 02:33:03 -0500

    Dev. checkpoint: Touchups.

--- specs/client-side/index.js
+++ specs/client-side/index.js
@@ -4,8 +4,8 @@
 QUnit.test( "it runs the code", function( assert ) {
   WWW_Applet.run([
     "focus on", ['#box_1'],
-    "add class", ['class_1']
+    "add class", ['weird']
   ]);
-  assert.ok( $('#box_1').hasClass('class_1') === true, "Passed!" );
+  assert.ok( $('#box_1').hasClass('weird') === true, "Passed!" );
 });
 
