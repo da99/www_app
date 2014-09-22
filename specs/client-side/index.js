37d79e0222e677d5905f88a62ea74af9305ea4ac
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-22 02:55:40 -0500

    Dev. checkpoint.

--- specs/client-side/index.js
+++ specs/client-side/index.js
@@ -9,3 +9,12 @@ QUnit.test( "it runs the code", function( assert ) {
   assert.ok( $('#box_1').hasClass('weird') === true, "Passed!" );
 });
 
+
+QUnit.test( "it evals the args as code", function( assert ) {
+  var o = WWW_Applet.run([
+    "add to stack", ["array", [1,2,3]]
+  ]);
+  assert.deepEqual( o.stack, [[1,2,3]], "Args are eval'd before run." );
+});
+
+
