d9d18805218c6c5bdd304453b21fe47812db38e5
da99 <i-hate-spam-1234567@mailinator.com>
2015-03-13 06:59:34 -0500

    Fixed: typos

--- specs/server-side/0011-id.rb
+++ specs/server-side/0011-id.rb
@@ -1,12 +1,12 @@
 
 
 
-describe :* do
+describe :id do
 
   it "raises Invalid if id has invalid chars" do
     should.raise(Escape_Escape_Escape::Invalid) {
       actual do
-        div.*('a<&a') { 'hello' }
+        div.id('a<&a') { 'hello' }
       end
     }.message.should.match /a<&a/
   end
@@ -14,8 +14,8 @@ describe :* do
   it "raises HTML_ID_Duplicate if id is used more than once" do
     should.raise(WWW_App::HTML_ID_Duplicate) {
       actual do
-        div.*(:my_id) { '1' }
-        div.*(:my_id) { '2' }
+        div.id(:my_id) { '1' }
+        div.id(:my_id) { '2' }
       end
     }.message.should.match /my_id/
   end
@@ -29,7 +29,7 @@ describe :* do
     target '<a id="warning" href="&#47;there">There</a>'
 
     actual do
-      a.*(:warning).href('/there') { "There" }
+      a.id(:warning).href('/there') { "There" }
     end
   end
 
